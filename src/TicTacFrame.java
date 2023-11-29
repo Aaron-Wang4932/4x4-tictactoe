241452d733a8a9d5160eab85cf7266cf213fd37c
Aaron Wang <aaronw4932@gmail.com>
2023-11-29 14:09:37 -0500

    design in progress

--- src/TicTacFrame.java
+++ src/TicTacFrame.java
@@ -1,20 +1,67 @@
+import javax.print.attribute.standard.MediaSize;
 import javax.swing.*;
 import java.awt.*;
 
 public class TicTacFrame extends JFrame {
+    // Images:
     final ImageIcon titleImg = new ImageIcon("resources/TicTacToe.png");
-    // Colours
+    // Colours:
     final Color NAVY_BLUE = new Color(0x2D3047); // Util. button colours
     final Color WHITE = new Color(0xFFFFFF); // Button Text, borders
     final Color BLUE = new Color(0x4A7AFF); // Tic Tac Toe buttons
     final Color PURPLE = new Color(0x7677FF); // Background
+
+    // JPanels:
+    JPanel ticTacToePanel;
+    JPanel whiteBGPanel;
+    JPanel titlePanel;
+    JPanel buttonPanel;
+
+    // JLabels:
+    JLabel titleLabel;
+
     public TicTacFrame() {
-        // Frame config:
-        super("Tic-Tac-Toe-Toc"); // Calls JFrame constructor, passing in the title.
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
-        getContentPane().setBackground(PURPLE);
-        setIconImage(titleImg.getImage());
+// ---- Frame config: ----------------------------------------
+        this.setTitle("Tic Tac Toe Toc!");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.getContentPane().setBackground(PURPLE);
+        this.setIconImage(titleImg.getImage());
+        this.setLayout(new BorderLayout());
+
+// ---- ticTacToePanel config: -------------------------------
+        ticTacToePanel = new JPanel();
+        ticTacToePanel.setLayout(new GridLayout(4, 4, 10, 10));
+        ticTacToePanel.setBackground(BLUE);
+
+        // whiteBGPanel config:
+        whiteBGPanel = new JPanel();
+        whiteBGPanel.setLayout(null);
+        whiteBGPanel.setBackground(WHITE);
+        whiteBGPanel.setPreferredSize(new Dimension(370, 370));
+        whiteBGPanel.add(ticTacToePanel);
+        ticTacToePanel.setBounds(10, 10, 350, 350);
+
+        // titleLabel config:
+        titleLabel = new JLabel("Tic Tac Toe.. Toc?");
+        titleLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+
+        // titlePanel config:
+        titlePanel = new JPanel();
+        titlePanel.setLayout(new FlowLayout());
+        titlePanel.add(titleLabel);
+        titlePanel.setBackground(PURPLE);
+
+        // buttonPanel config:
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.setBackground(PURPLE);
+
+
+        // Adding Components:
+        this.add(whiteBGPanel, BorderLayout.CENTER);
+        this.add(titlePanel, BorderLayout.NORTH);
+        this.add(buttonPanel, BorderLayout.SOUTH);
 
 //      pack(); // Resizes JFrame to perfectly include all components.
         setLocationRelativeTo(null); // Centres the JFrame.
